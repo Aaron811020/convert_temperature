e2cb88e87a99ca756cfbdfc37e563a67893d9dfa
AaronYen <clove2471@gmail.com>
2019-05-09 21:27:47 +0800

    Add leave programing

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 #溫度轉換
 
-option = input("1.攝氏轉華氏 2.華氏轉攝氏")
+option = input("1.攝氏轉華氏 2.華氏轉攝氏 3.離開")
 
 
 if option == "1":
 	old_degree = float(input("請輸入攝氏溫度"))
 	new_degree = old_degree * 1.8 + 32
-else:
+	print("溫度:", new_degree)
+elif option == "2":
 	old_degree = float(input("請輸入華氏溫度"))
 	new_degree = (old_degree - 32) * 5 / 9
+	print("溫度:", new_degree)
+else:
+	print("離開程式")
 
-print("溫度:", new_degree)
+
